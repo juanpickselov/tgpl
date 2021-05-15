b0f973c4fe4e09106e366ccc2741e58827f6e38c
juanpickselov <juanpickselov@sbcglobal.net>
2021-05-15 14:52:48 -0400

    exercise 1.2 want to find another way without adding all the \n

---
+++ ch01/echo2ex1p2.go
@@ -0,0 +1,13 @@
+// Echo2 prints its command-line args args args.package ch01
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	for indexNum, arg := range os.Args[1:] {
+		fmt.Printf("%d " + arg +"\n", indexNum)
+	}
+}
