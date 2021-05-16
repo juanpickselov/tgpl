5c0b69f828aedd19819d880575bc73b44782d662
juanpickselov <juanpickselov@sbcglobal.net>
2021-05-15 21:23:22 -0400

    added the solution I admit I found on torbiak's github repo

--- ch01/dup2ex1p4.go
+++ ch01/dup2ex1p4.go
@@ -9,9 +9,10 @@ import (
 
 func main() {
     counts := make(map[string]int)
+    fileList := make(map[string][]string)
     files := os.Args[1:]
     if len(files) == 0 {
-        countLines(os.Stdin, counts)
+        countLines(os.Stdin, counts, fileList)
     } else {
         for _, arg := range files {
             fileToCheck, err := os.Open(arg)
@@ -19,21 +20,34 @@ func main() {
                 fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
                 continue
             }
-            countLines(fileToCheck, counts)
+            countLines(fileToCheck, counts, fileList)
             fileToCheck.Close()
         }
     }
     for line, n := range counts {
         if n > 1 {
-            fmt.Printf("%d\t%s\n", n, line)
+            fmt.Printf("%d\t%v\t%s\n", n, fileList[line], line)
         }
     }
 }
 
-func countLines(fileToCheck *os.File, counts map[string]int) {
+func in(needle string, strings []string) bool {
+	for _, aString := range strings {
+		if needle == aString {
+			return true
+		}
+	}
+	return false
+}
+
+func countLines(fileToCheck *os.File, counts map[string]int, fileList map[string][]string) {
     input := bufio.NewScanner(fileToCheck)
     for input.Scan() {
+        line := input.Text()
         counts[input.Text()]++
+        if !in(fileToCheck.Name(), fileList[line]) {
+            fileList[line] = append(fileList[line], fileToCheck.Name())
+        }
     }
     // NOTE: ignoring potential errors from input.Err()
 }
